769c3053acdbef198dfbfd4d5435706230982cc4
zxt1996 <15625144271@163.com>
2020-11-10 20:34:50 +0800

    interface接口

--- demo4.ts
+++ demo4.ts
@@ -1,4 +1,5 @@
 // interface接口
+// 接口只是在 TypeScript 里帮我们作语法校验的工具
 
 interface Girl {
     name: string;
@@ -28,11 +29,32 @@ const screenResume = (girl: Girl) => {
 // 类型别名可以直接给类型，比如string，而接口必须代表对象。
 type Girl1 = string;
 
-// 接口非必选值
-// 在:号前加一个?
 interface Girl2 {
     name: string;
     age: number;
     bust: number;
+    // 接口非必选值
+    // 在:号前加一个?
     waistline?: number;
+    // 允许加入任意值
+    // 属性的名字是字符串类型，属性的值可以是任何类型
+    [propname: string]: any;
+    // 接口里的方法
+    // 返回值是string类型
+    say(): string;
+}
+
+// 接口和类的约束
+class XiaoGirl implements Girl2 {
+  name = "j";
+  age = 12;
+  bust = 90;
+  say() {
+    return "jjj";
+  }
+}
+
+// 接口间的继承
+interface Teacher extends Girl2 {
+  teach(): string;
 }

0d78da1504233cd258d6b832903be966b64dbb0c
zxt1996 <15625144271@163.com>
2020-11-20 21:56:22 +0800

    函数

--- demo2.ts
+++ demo2.ts
@@ -25,3 +25,54 @@ function getNumber({ one }: {one: number}): number {
 }
 
 const one = getNumber({one: 1})
+
+// 可选参数
+function foo(bar: number, other: string = 'hello', bas?: string): void {
+    console.log(bar, other);
+    if (bas) {
+        console.log(bas);
+    }
+}
+
+foo(123);
+foo(123, 'one', 'two');
+
+
+interface IUser {
+    name: string,
+    id: number,
+    address: string
+}
+// 重载
+// 使用相同名称和不同参数数量或类型创建多个方法的一种能力
+// 上边是声明
+function getNames(userList: IUser[], id: number): string;
+function getNames(userList: IUser[], id: number[]): string[];
+// 下边是实现
+function getNames(userList: IUser[], id: number | number[]): string | string[] {
+    if (Array.isArray(id)) {
+        const names: string[] = [];
+        userList.forEach((user: IUser) => {
+            if (id.includes(user.id)) {
+                names.push(user.name);
+            }
+        })
+
+        return names;
+    }
+
+    const found = userList.find((user: IUser) => user.id === id);
+    if (found) {
+        return found.name;
+    } else {
+        return ''
+    }
+}
+
+const users: IUser[] = [
+    {name: 'John', id: 1, address: ''},
+    {name: 'Joi', id: 2, address: ''},
+    {name: 'Kevin', id: 3, address: ''}
+  ]
+  const John = getNames(users, 1);
+  const JohnAndKevin = getNames(users, [1, 3]);

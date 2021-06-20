69c0c69ed4c976934ca2265a3e18e85bc7a62f54
zxt1996 <15625144271@163.com>
2021-06-20 17:36:43 +0800

    as const 在自定义 Hooks 中的使用

---
+++ ts-with-react/src/hooks/useLoading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function useLoading() {
+  const [isLoading, setState] = React.useState(false);
+  const load = (aPromise: Promise<any>) => {
+    setState(true);
+    return aPromise.finally(() => setState(false));
+  };
+  return [isLoading, load] as const; // infers [boolean, typeof load] instead of (boolean | typeof load)[]
+}
